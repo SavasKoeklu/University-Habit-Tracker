2d23af4bec45df42cb3a0ee65b8ca6d5f9b865fa
savaskoeklue <savaskoeklue@outlook.de>
2024-08-15 23:37:08 +0200

    close #1

--- Habit-Tracker/src/pages/index.vue
+++ Habit-Tracker/src/pages/index.vue
@@ -1,7 +1,51 @@
 <template>
-  <HelloWorld />
+<v-text-field 
+   hide-details="auto"
+   label="Hours Worked"
+   type="date"
+   v-model="formData.date"
+   >
+</v-text-field>
+<v-text-field 
+   hide-details="auto"
+   label="Hours Worked"
+   v-model="formData.hours"
+   >
+</v-text-field>
+
+<v-btn @click="submit">Add Entry</v-btn>
+
+<v-card title="Habit Table">
+
+    <v-data-table
+    :items="items"
+    ></v-data-table>
+  </v-card>
 </template>
 
-<script lang="ts" setup>
-  //
+
+<script lang="ts">
+
+export default defineComponent({
+    data: () => ({
+        formData: {
+            date: null,
+            hours: null,
+        },
+        items:[
+            
+        ] as Object[]
+    }),
+    methods:{
+        submit(){
+           
+            this.items.push(JSON.parse(JSON.stringify(this.formData)))
+            console.log(this.items)
+        
+        }
+    }
+})
+
+
+
 </script>
